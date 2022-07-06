4cf0259ec9cf8d6b0394458e919e21c4f3da824c
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-07-07 01:39:47 +0530

    Code Refactoring

--- algorithm.py
+++ algorithm.py
@@ -5,6 +5,9 @@ import cv2
 image = cv2.imread("Prototype.png");
 
 #### Describing Parameters
+# Text to be printed on the certificate
+text = "Sample Name"
+
 # Font
 font = cv2.FONT_HERSHEY_DUPLEX
 
@@ -12,13 +15,18 @@ font = cv2.FONT_HERSHEY_DUPLEX
 fontScale = 5
 
 # Color in RGB
-color = (255, 0, 0)
+'''
+Note:
+Color format in OpenCV is BRG and not RGB
+When you add a custom color... Please take care of this thing
+'''
+color = (236, 235, 250)
   
 # Thichness of the Text
-thickness = 2
+thickness = 3
    
 # Getting the text size
-textsize = cv2.getTextSize("Sample Name", font, fontScale, thickness)[0]
+textsize = cv2.getTextSize(text.capitalize()  , font, fontScale, thickness)[0]
 
 # Setting the coordinates 
 '''
@@ -37,7 +45,7 @@ textX = round((image.shape[1] - textsize[0]) / 2)
 textY = round((image.shape[0] + textsize[1]) / 2)
 
 # Finally putting the text on the center of the image.
-image = cv2.putText(image, 'Sample Name', (textX , textY), font, fontScale, color, thickness, cv2.LINE_AA)
+image = cv2.putText(image, text.capitalize() , (textX , textY), font, fontScale, color, thickness, cv2.LINE_AA)
 
 # Saving this as a new image
 cv2.imwrite("Testing.jpg", image)
