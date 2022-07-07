9128b755bcec5486260d4d616192a6afe1aff44d
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-07-07 14:37:30 +0530

    Declaring class for printing names on certificate

--- algorithm.py
+++ algorithm.py
@@ -1,36 +1,31 @@
 # Getting nessesary libraries
 import cv2
 
-# Getting the certificate as an Image
-image = cv2.imread("Prototype.png");
+# Implementing the class to print the name on the certificate
 
-#### Describing Parameters
-# Text to be printed on the certificate
-text = "Sample Name"
+class PrintNames:
+    
+    def __init__(self , image , text , font , fontScale , color_BRG , thickness , finalName):
+        
+        textsize = cv2.getTextSize(str.title(text)  , font, fontScale, thickness)[0];
+        # print(textsize);
+        textX = round((image.shape[1] - textsize[0]) / 2)
+        textY = round((image.shape[0] + textsize[1]) / 2)
+        
+        image = cv2.putText(image, str.title(text) , (textX , textY), font, fontScale, color_BRG, thickness)
 
-# Font
-font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.imwrite(finalName , image);
+        cv2.waitKey(0);
 
-# Font Scale
-fontScale = 5
-
-# Color in RGB
+    
 '''
-Note:
+Note #1:
 Color format in OpenCV is BRG and not RGB
 When you add a custom color... Please take care of this thing
 '''
-color = (236, 235, 250)
-  
-# Thichness of the Text
-thickness = 3
-   
-# Getting the text size
-textsize = cv2.getTextSize(text.capitalize()  , font, fontScale, thickness)[0]
 
-# Setting the coordinates 
 '''
-Note:
+Note #2:
 The textsize function returns a tuple as follows:
 (width_of_the_text , height_of_the_text , baseline)
 
@@ -41,16 +36,13 @@ So, the variables below is the difference in the width and height of both image 
 The round function is mandatory since pixels cant' be floating numbers
 '''
 
-textX = round((image.shape[1] - textsize[0]) / 2)
-textY = round((image.shape[0] + textsize[1]) / 2)
+'''
+Note #4:
+Add the file extension at the end of the filename
+So that the file is saved in the image format.
+'''
 
-# Finally putting the text on the center of the image.
-image = cv2.putText(image, text.capitalize() , (textX , textY), font, fontScale, color, thickness, cv2.LINE_AA)
+# >>>>>>>>> Testing the Class
 
-# Saving this as a new image
-cv2.imwrite("Testing.jpg", image)
-
-# Printing the image
-# cv2.imshow("Names" , image);
-
-cv2.waitKey(0);
+image = cv2.imread("Prototype.png");
+Sample = PrintNames(image , "Ishaan Joshi" , cv2.FONT_HERSHEY_DUPLEX , 4 , (236, 235, 250) , 2 , "joshi.ishaan.2001@gmail.com.png");
